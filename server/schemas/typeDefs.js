154f645574dd2c3cedc996b7df6f70d5036823a3
Vincent Baraga <vincentb.baraga@gmail.com>
2024-01-30 18:45:46 -0500

    addes initial queries

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+const typeDefs = `#graphql
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    posts: [Post]!
+  }
+
+  type Post {
+    _id: ID
+    postText: String
+    postAuthor: String
+    createdAt: String
+    comments: [Comment]!
+  }
+
+  type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+    posts(username: String): [Post]
+    post(postId: ID!): Post
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addPost(postBody: String!, postAuthor: String!): Post
+    addComment(
+      postId: ID!
+      commentText: String!
+      commentAuthor: String!
+    ): Post
+    removePost(postId: ID!): Post
+    removeComment(postId: ID!, commentId: ID!): Post
+  }
+`;
+
+module.exports = typeDefs;
