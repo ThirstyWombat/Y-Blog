08c72825c27579f234b5b4917b5a129d5d3b6bf3
Vincent Baraga <vincentb.baraga@gmail.com>
2024-02-10 15:04:46 -0500

    added unfinished me query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,43 +1,36 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const GET_POSTS = gql`
-	{
-		posts {
-			_id
-			author {
-				_id
-				username
-				email
-			}
-			postBody
-			createdAt
-			comments {
-				_id
-				commentAuthor
-				commentText
-				createdAt
-			}
-		}
-	}
+  {
+    posts {
+      _id
+      author {
+        _id
+        username
+        email
+      }
+      postBody
+      createdAt
+      comments {
+        _id
+        commentAuthor
+        commentText
+        createdAt
+      }
+    }
+  }
 `;
 
 export const GET_USER_POSTS = gql`
-	{
-		user {
-			_id
-			author {
-				_id
-				username
-				email
-			}
-			postBody
-			createdAt
-			comments {
-				_id
-				commentAuthor
-				commentText
-				createdAt
-			}
-		}
-	}
+  {
+    me {
+      _id
+      username
+      posts {
+        _id
+        postBody
+        createdAt
+      }
+    }
+  }
 `;
