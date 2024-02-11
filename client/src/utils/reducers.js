3cb82071844a539e6ffa377252099d4a013490a4
Vincent Baraga <vincentb.baraga@gmail.com>
2024-02-11 13:23:31 -0500

    fixed a token error in post componenet

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,30 +1,30 @@
-import { UPDATE_POST, DELETE_POST, ADD_POST } from './actions';
+// import { UPDATE_POST, DELETE_POST, ADD_POST } from './actions';
 
-export const reducer = (state, action) => {
-	switch (action.type) {
-		case ADD_POST:
-			return {
-				...state,
-				posts: [...action?.post],
-			};
-		case UPDATE_POST:
-			return {
-				...state,
-				posts: [...action?.post],
-			};
-		case DELETE_POST:
-			let newState = state.posts?.filter((post) => {
-				return post._id !== action._id;
-			});
+// export const reducer = (state, action) => {
+// 	switch (action.type) {
+// 		case ADD_POST:
+// 			return {
+// 				...state,
+// 				posts: [...action?.post],
+// 			};
+// 		case UPDATE_POST:
+// 			return {
+// 				...state,
+// 				posts: [...action?.post],
+// 			};
+// 		case DELETE_POST:
+// 			let newState = state.posts?.filter((post) => {
+// 				return post._id !== action._id;
+// 			});
 
-			return {
-				...state,
-				posts: newState,
-			};
+// 			return {
+// 				...state,
+// 				posts: newState,
+// 			};
 
-		// Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
-		// This saves us from a crash.
-		default:
-			return state;
-	}
-};
+// 		// Return the state as is in the event that the `action.type` passed to our reducer was not accounted for by the developers
+// 		// This saves us from a crash.
+// 		default:
+// 			return state;
+// 	}
+// };
